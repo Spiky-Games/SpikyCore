2a7d48a0d47c953ac97c9b88bd3ad64a4fe1e9df
Mateo Javier Bonnin <mateojavierbonnin@gmail.com>
2022-04-26 15:50:03 -0300

    - Implemented new prev and next level reload.

--- GenericCore/Scripts/GameManager.cs
+++ GenericCore/Scripts/GameManager.cs
@@ -3,22 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private LevelDefinitionScriptable levelDefinitionScriptable;
-
     public static GameManager Instance
     {
         get;
         set;
     }
 
-    public void Start()
+    private void Awake()
     {
         Instance = this;
+    }
+
+    public void Start()
+    {
+        this.Bind();
         this.StartLevel();
     }
 
+    private void OnDestroy()
+    {
+        this.Unbind();
+    }
+
+    private void Bind()
+    {
+        DefaultGameEventSystemManager.Instance.GameEventSystem.AddEventListener<ForceReloadLevelEvent>(this.ForceReloadLevelEventHandler);
+    }
+
+    private void Unbind()
+    {
+        if (DefaultGameEventSystemManager.Instance != null)
+        {
+            DefaultGameEventSystemManager.Instance.GameEventSystem.RemoveEventListener<ForceReloadLevelEvent>(this.ForceReloadLevelEventHandler);
+        }
+    }
+
+    private void ForceReloadLevelEventHandler(ForceReloadLevelEvent forceReloadLevelEvent)
+    {
+        this.LoadCurrentLevel();
+    }
+
     private void StartLevel()
+    {
+        //this.LoadCurrentLevel();
+    }
+
+    private void LoadCurrentLevel()
     {
         LevelDefinition levelDefinition = DefaultLevelManager.Instance.LoadLevel(DefaultUserManager.Instance.UserData.CurrentLevel);
         SceneManager.LoadScene(levelDefinition.sceneToLoad.name);
