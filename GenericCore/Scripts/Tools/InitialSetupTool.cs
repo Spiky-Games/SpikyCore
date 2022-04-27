f6db17705db81aeeab974635da6d350daed72c2e
Mateo Javier Bonnin <mateojavierbonnin@gmail.com>
2022-04-27 16:44:24 -0300

    - Added replace prefab tool and made a new tool to setup initial ios and player settings.

---
+++ GenericCore/Scripts/Tools/InitialSetupTool.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+public class InitialSetupTool : EditorWindow
+{
+    private const string COMPANY_NAME = "spiky";
+
+    [MenuItem("Tools/Initial Setup")]
+    private static void StartInitialSetupTool()
+    {
+        PlayerSettings.companyName = COMPANY_NAME;
+        PlayerSettings.accelerometerFrequency = 0;
+        PlayerSettings.applicationIdentifier = "";
+        PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.iOS, ApiCompatibilityLevel.NET_4_6);
+        PlayerSettings.allowedAutorotateToLandscapeRight = false;
+        PlayerSettings.allowedAutorotateToLandscapeLeft = false;
+        PlayerSettings.gpuSkinning = true;
+
+        PlayerSettings.SplashScreenLogo[] logos = new PlayerSettings.SplashScreenLogo[1];
+        Sprite companyLogo = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/SpikyCore/GenericCore/Art/Spiky_Games_Logo.png", typeof(Sprite));
+        logos[0] = PlayerSettings.SplashScreenLogo.Create(2, companyLogo);
+        PlayerSettings.SplashScreen.logos = logos;
+    }
+}
