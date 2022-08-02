2076e8820be24c1d82a735ce201f3057fbd6289b
Lucas de Hoz <dehozlucas@gmail.com>
2022-08-02 12:23:29 -0300

    Removed URP from base proyect

--- GenericCore/Scripts/Tools/Editor/InitialProjectStructure.cs
+++ GenericCore/Scripts/Tools/Editor/InitialProjectStructure.cs
@@ -3,13 +3,11 @@ using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 namespace SpikyCoreInitializer
 {
     public class InitialProjectStructure
     {
-         
         [MenuItem("Spiky Tools/Create Initial Folders")]
         public static void SetupInitialFolders()
         {
@@ -17,27 +15,6 @@ namespace SpikyCoreInitializer
             FolderUtilities.CreateGroupOfFolder(gameFolder, new []{"Art","Scripts","Materials","Scenes","Prefabs"});
         }
         
-        [MenuItem("Spiky Tools/URP/Setup Post Processing")]
-        public static void SetupPostProcessing()
-        {
-            List<VolumeProfile> volumeProfiles = FindAssetsByType<VolumeProfile>();
-            volumeProfiles.ForEach(volumeProfile =>
-            {
-                if (!volumeProfile.TryGet(out ColorAdjustments colorAdjustments))
-                {
-                    colorAdjustments = VolumeProfileFactory.CreateVolumeComponent<ColorAdjustments>(volumeProfile);
-                }
-                colorAdjustments.contrast.overrideState = true;
-                colorAdjustments.saturation.overrideState = true;
-                
-                Debug.Log($"{AssetDatabase.GetAssetPath(volumeProfile)} Has Changed.");
-                EditorUtility.SetDirty(volumeProfile);
-                
-                AssetDatabase.SaveAssetIfDirty(volumeProfile);
-                Selection.activeObject = volumeProfile;
-            });
-        }
-
         private static List<T> FindAssetsByType<T>() where T : UnityEngine.Object
         {
             List<T> assets = new List<T>();
