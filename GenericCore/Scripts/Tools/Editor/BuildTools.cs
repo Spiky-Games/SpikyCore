56175552d4b934281ec94c0d5c14c866cd368613
cscaliaSpiky <cscaliaspiky@gmail.com>
2022-08-22 13:52:45 -0300

    Build Game Tools Modified

--- GenericCore/Scripts/Tools/Editor/BuildTools.cs
+++ GenericCore/Scripts/Tools/Editor/BuildTools.cs
@@ -1,36 +1,55 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using System.IO;
+using Codice.Client.BaseCommands;
 
 namespace SpikyCoreInitializer
 {
     public class BuildTools
     {
         private static string OUTPUT_PREF_KEY => "BuildOutputPath_"+ Application.productName;
+        private static string BUILD_PATH_PREF_KEY => "BuildPath_"+ Application.productName;
+        
+        public static string BuildPath
+        {
+            get => PlayerPrefs.GetString(BUILD_PATH_PREF_KEY);
+            set => PlayerPrefs.SetString(BUILD_PATH_PREF_KEY, value);
+        }  
 
         [MenuItem("Spiky Tools/Build/With Debug")]
         public static void BuildGame ()
         {
-            // Get Output Build Path.
+            string outputPath = GetOrCreateBuildPath();
+
+            var options = new BuildPlayerOptions();
+            BuildPlayerOptions playerSettings = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(options);
+            
+            AddDefineSymbols(debugSymbols);
+            
+            BuildPipeline.BuildPlayer(playerSettings);
+            BuildPath = playerSettings.locationPathName;
+            RemoveDefineSymbols(debugSymbols);
+            
+            string finalPath = outputPath + $"/{Application.productName}.apk";
+            File.Delete(finalPath);
+            File.Copy(BuildPath, finalPath);
+            
+            AssetDatabase.SaveAssets();
+        }
+
+        private static string GetOrCreateBuildPath()
+        {
             string outputPath = EditorPrefs.GetString(OUTPUT_PREF_KEY, "");
             if (string.IsNullOrEmpty(outputPath))
             {
                 outputPath = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
                 EditorPrefs.SetString(OUTPUT_PREF_KEY, outputPath);
             }
-            
-            string[] levels = EditorBuildSettings.scenes.Select(s => s.path).ToArray();
-            
-            AddDefineSymbols(debugSymbols);
-            
-            // Build player.
-            BuildPipeline.BuildPlayer(levels, outputPath + $"/{Application.productName}.apk",BuildTarget.Android, BuildOptions.None);
-            
-            
-            RemoveDefineSymbols(debugSymbols);
-            
-            AssetDatabase.SaveAssets();
+
+            return outputPath;
         }
 
         [MenuItem("Spiky Tools/Build/Whats My Build Path?")]
@@ -40,6 +59,20 @@ namespace SpikyCoreInitializer
             Debug.Log(outputPath);
         }
         
+        [MenuItem("Spiky Tools/Build/DebugMenu/Add")]
+        public static void AddDebugMenu ()
+        {
+            AddDefineSymbols(debugSymbols);
+            AssetDatabase.SaveAssets();
+        }
+        
+        [MenuItem("Spiky Tools/Build/DebugMenu/Remove")]
+        public static void RemoveDebugMenu ()
+        {
+            RemoveDefineSymbols(debugSymbols);
+            AssetDatabase.SaveAssets();
+        }
+
         [MenuItem("Spiky Tools/Build/Clear Output Path")]
         public static void ClearOutputPath()
         {
